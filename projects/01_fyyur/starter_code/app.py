a8f3fd73f80349b36cdd02043b1c98503a281d40
Tanada Daisuke <nvkx873110@gmail.com>
2020-05-06 18:08:05 +0900

    modify venue array

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -42,7 +42,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.Array(db.String))
+    genres = db.Column(db.ARRAY(db.String))
     webiste = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
