7175ca5b2c694e8f67b578c11de31f23e209c6c9
Tanada Daisuke <nvkx873110@gmail.com>
2020-06-20 22:28:31 +0900

    remove artist search comment

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -341,6 +341,7 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  # -> DONE.
   response_term = request.form.get('search_term')
   response = {}
   artists = Artist.query.filter(Artist.name.ilike('%' + response_term + '%'))
@@ -357,14 +358,6 @@ def search_artists():
     artist_item['num_upcoming_shows'] = shows.count()
     data.append(artist_item)
   response['data'] = data
-  # response={
-  #   "count": 1,
-  #   "data": [{
-  #     "id": 4,
-  #     "name": "Guns N Petals",
-  #     "num_upcoming_shows": 0,
-  #   }]
-  # }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
