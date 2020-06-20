6a223c0fd3e57a303b1c2609dbe0c8d441db66f2
Tanada Daisuke <nvkx873110@gmail.com>
2020-06-20 22:27:05 +0900

    update artist search controller

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -341,14 +341,30 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  response_term = request.form.get('search_term')
+  response = {}
+  artists = Artist.query.filter(Artist.name.ilike('%' + response_term + '%'))
+  count = artists.count()
+  response['count'] = count
+  data = []
+  for artist in artists:
+    artist_item = {}
+    id = artist.id
+    name = artist.name
+    artist_item['id'] = id
+    artist_item['name'] = name
+    shows = Show.query.filter(Show.artist_id == artist.id).filter(Show.time >= datetime.utcnow())
+    artist_item['num_upcoming_shows'] = shows.count()
+    data.append(artist_item)
+  response['data'] = data
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
