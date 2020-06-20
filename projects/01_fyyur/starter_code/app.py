2a2fab397de204383c61b2e8c0cd050dead64117
Tanada Daisuke <nvkx873110@gmail.com>
2020-06-20 22:12:37 +0900

    remove venue search comment

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -157,6 +157,7 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  # -> DONE
   response = {}
   response_term = request.form.get('search_term')
   venues = Venue.query.filter(Venue.name.ilike('%' + response_term + '%'))
@@ -173,14 +174,6 @@ def search_venues():
     venue_item['num_upcoming_shows'] = shows.count()
     data.append(venue_item)
   response['data'] = data
-  # response={
-  #   "count": 1,
-  #   "data": [{
-  #     "id": 2,
-  #     "name": "The Dueling Pianos Bar",
-  #     "num_upcoming_shows": 0,
-  #   }]
-  # }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
