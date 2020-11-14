27a8ed9fd30b080852b957c11ded6a5107428e3d
Tanada Daisuke <nvkx873110@gmail.com>
2020-11-14 17:18:04 +0900

    handling auth error class with flask server

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -129,3 +129,10 @@ def unprocessable(error):
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+@app.errorhandler(AuthError)
+def auth_error(error):
+    return jsonify({
+        'success': False,
+        'error': error.status_code,
+        'message': error.error['code']
+    }), error.status_code
